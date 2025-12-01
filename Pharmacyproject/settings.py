62ce62370801dbac9ae797572be79cba0bc71a66
Maz1Urek <urekmaz470@gmail.com>
2025-12-01 12:00:50 +0530

    Configure Postgres for Render

--- Pharmacyproject/settings.py
+++ Pharmacyproject/settings.py
@@ -143,3 +143,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
